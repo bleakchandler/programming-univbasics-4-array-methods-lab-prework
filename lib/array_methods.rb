3dd1822c4754c89b761ebac021307e3ab722a257
Bleak Chandler <bleakchandler@gmail.com>
2021-01-18 23:01:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def using_include(array, element)
  newarray = ["wow", "I", "am", "really", "learning", "arrays!"]
-
- newarray.include?("wow")
+newarray.include? ("wow")
+ newarray.include?("Tardis")
   
 end
 
