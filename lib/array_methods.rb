23386b1d4947a6adba57dfdd968accd3c5e6b455
Bleak Chandler <bleakchandler@gmail.com>
2021-01-18 22:56:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,24 +1,24 @@
 def using_include(array, element)
- newarray = ["wow", "these", "arrays!"]
+ newarray = ["wow", "these", "arrays!", "are", "so", "crazy"]
  newarray.include?("arrays!")
 end
 
 def using_sort(array)
-newarray = ["wow", "these", "arrays!"]
+newarray = ["wow", "these", "arrays!", "are", "so", "crazy"]
 newarray.sort
 end
 
 def using_reverse(array)
-newarray = ["wow", "these", "arrays!"]
+newarray = ["wow", "these", "arrays!", "are", "so", "crazy"]
 newarray.reverse
 
 def using_first(array)
-newarray = ["wow", "these", "arrays!"]
+newarray = ["wow", "these", "arrays!", "are", "so", "crazy"]
 newarray.first
 end
 
 def using_last(array)
-newarray = ["wow", "these", "arrays!"]
+newarray = ["wow", "these", "arrays!", "are", "so", "crazy"]
 newarray.last
 end
 
