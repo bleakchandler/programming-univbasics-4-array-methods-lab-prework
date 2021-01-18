a72b2fe5365ce31ff8e257bfc91ec3f69acbd39c
Bleak Chandler <bleakchandler@gmail.com>
2021-01-18 22:52:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+array = ["wow", "these", "arrays!"]
+array.include?("arrays!")
 end
 
 def using_sort(array)
-
+array = ["wow", "these", "arrays!"]
+array.sort
 end
 
 def using_reverse(array)
-
-end
+array = ["wow", "these", "arrays!"]
+array.reverse
 
 def using_first(array)
-
+array = ["wow", "these", "arrays!"]
+array.first
 end
 
 def using_last(array)
-
+array = ["wow", "these", "arrays!"]
+array.last
 end
 
 def using_size(array)
-
+array = ["wow", "these", "arrays!, "are", "so", "crazy"]
+array.last
 end
