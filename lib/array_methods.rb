18a2a5856330a8ea1ada9239d20e654119d69e01
Bleak Chandler <bleakchandler@gmail.com>
2021-01-18 23:00:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,29 +1,29 @@
 def using_include(array, element)
- newarray = ["wow", "these", "arrays!", "are", "so", "crazy"]
+ newarray = ["wow", "I", "am", "really", "learning", "arrays!"]
  newarray.include?("arraysd!")
 end
 
 def using_sort(array)
-newarray = ["wow", "these", "arrays!", "are", "so", "crazy"]
+newarray = ["wow", "I", "am", "really", "learning", "arrays!"]
 newarray.sort
 end
 
 def using_reverse(array)
-newarray = ["wow", "these", "arrays!", "are", "so", "crazy"]
+newarray = ["wow", "I", "am", "really", "learning", "arrays!"]
 newarray.reverse
 end
 
 def using_first(array)
-newarray = ["wow", "these", "arrays!", "are", "so", "crazy"]
+newarray = ["wow", "I", "am", "really", "learning", "arrays!"]
 newarray.first
 end
 
 def using_last(array)
-newarray = ["wow", "these", "arrays!", "are", "so", "crazy"]
+newarray = ["wow", "I", "am", "really", "learning", "arrays!"]
 newarray.last
 end
 
 def using_size(array)
-newarray = ["wow", "these", "arrays!", "are", "so", "crazy"]
+newarray = ["wow", "I", "am", "really", "learning", "arrays!"]
 newarray.size
 end
