526a2a44b03e40891f92ec0743b1ccb0334d4697
Bleak Chandler <bleakchandler@gmail.com>
2021-01-18 23:01:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,8 @@
 def using_include(array, element)
  newarray = ["wow", "I", "am", "really", "learning", "arrays!"]
+newarray.include?("Mario")
  newarray.include?("wow")
-  newarray.include?("Mario")
+  
 end
 
 def using_sort(array)
